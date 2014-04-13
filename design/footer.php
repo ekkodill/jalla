ef8fe66ae560a0c307211509f9451b6c04e1721f
Mupetmedia <erik.bjornflaten@gmail.com>
2014-04-13 10:13:42 +0200

    Stemmer det.

--- design/footer.php
+++ design/footer.php
@@ -1,12 +1,17 @@
 <!--Denne siden er utviklet av Dag-Roger Eriksen., siste gang endret 27.03.2014
 Denne siden er kontrollert av Erik Bjørnflaten siste gang 30.03.2014  !-->
 
+
  <footer>
+
+
+ <img class="logoforslag"src="img/logo_forslag.png" height="180" width="180">
     <h3>Touch - Utviklingsoppgave 2014</h3>
     <h5>laget av</h5>
     <h4>Gruppe 3</h4>
 
 	<table id="gruppe">
+
 		<tr>
 			<td>Kurt Alexander Aamodt</td>
 			<td>Erik Bjørnflaten</td>
