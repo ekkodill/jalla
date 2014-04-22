fb5e35d5c132a35290fd3b7f4731ed7d64801dd3
ekkodill <rambo89@gmail.com>
2014-04-22 13:42:48 +0200

    More fixes

--- minside.php
+++ minside.php
@@ -4,8 +4,8 @@ Denne siden er kontrollert av Kurt A. Amodt siste gang 30.03.2014 !-->
 $db = getDB();
 
 
+//Oppdaterer brukerens informasjon på minside
 if(!empty($_POST['updateinfo'])) {
-
   if(isset($_POST['upfnavn'],$_POST['upenavn'], $_POST['upepost'])) {
   $brukerPK = $user_data['brukerPK'];
   $fnavn = sanitize(trim($_POST['upfnavn']));
@@ -20,13 +20,10 @@ if(!empty($_POST['updateinfo'])) {
 }
 
 
-
-
 //Sjekker at boksene er utfyllt og at informasjonen stemmer for så å oppdatere til det nye passordet.
 if(isset($_POST['nypw'])) {
     $gammeltpw = passord(sanitize($_POST['oldpassword']));
     $nyttpassord = passord(sanitize($_POST['password']));
-
 if (isset($_POST['password']) === true && isset($_POST['oldpassword']) === true && empty($_POST['passwordcheck']) === false) {
 if($user_data['passord'] != $gammeltpw) {
     $errors[] = 'Gammelt passord stemmer ikke';
@@ -40,8 +37,6 @@ if($user_data['passord'] != $gammeltpw) {
         } else { $errors[] = 'Alle feltene m&aring; fylles inn'; }
 } 
 ?>
-
-
 <!doctype html>
 <html>
 <?php include('design/footer.php'); 
@@ -77,17 +72,19 @@ if($user_data['passord'] != $gammeltpw) {
                 document.getElementById('updt').click();
               }
           </script>
+          <!--Elemeneter for å oppdatere brukerens profil!-->
         <div id="page">
           <section>
-          <div class="msvenstre"><div class="bpbilde"><img src="img/mann.jpg" height:"30%" width="70%"alt="bilde"></div>
-          <div class="profinfo">
-          <form name="profil" action="minside.php" method="POST">
-          <label>Fornavn:</label><input type="text" name="upfnavn" class="minsinputfor" id="fornavn" value="<?php echo $user_data['fornavn'] ?>"/><br>
-          <label>Etternavn:</label><input type="text" name="upenavn" class="minsinputett" id="etternavn" value="<?php echo $user_data['etternavn'] ?>"/><br>
-          <label>E-post:</label><input type="text" name="upepost" class="minsinputepo" id="epost" value="<?php echo $user_data['ePost'] ?>"/><br>
-          <input type='submit' id="updt" name="updateinfo" value="Oppdater"/><br />
-          </form>
+            <div class="msvenstre"><div class="bpbilde"><img src="img/mann.jpg" height:"30%" width="70%"alt="bilde"></div>
+              <div class="profinfo">
+                <form name="profil" action="minside.php" method="POST">
+                  <label>Fornavn:</label><input type="text" name="upfnavn" class="minsinputfor" id="fornavn" value="<?php echo $user_data['fornavn'] ?>"/><br>
+                  <label>Etternavn:</label><input type="text" name="upenavn" class="minsinputett" id="etternavn" value="<?php echo $user_data['etternavn'] ?>"/><br>
+                  <label>E-post:</label><input type="text" name="upepost" class="minsinputepo" id="epost" value="<?php echo $user_data['ePost'] ?>"/><br>
+                  <input type='submit' id="updt" name="updateinfo" value="Oppdater"/><br />
+                </form>
             <?php 
+            //Utskrift for statusmeldinger når brukeren oppdaterer informasjonen sin
             if(isset($_GET['oppdatert'])) {
               echo "Oppdatert";
             } elseif(isset($_GET['error'])) {
@@ -96,23 +93,18 @@ if($user_data['passord'] != $gammeltpw) {
               echo "Det oppstod en feil, et eller flere felt kan ikke være tomme";
             }
 
-
-
            ?>
+        </div>
           </div>
-        
-          </div>
-       
-         <form class="fasplas"action="minside.php" method="post">
-            <select class="dropned" name='minsideoppgli' onchange="this.form.submit();">
-            <option class="dropned" name="ubesvoppg"     value='ubesvoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'ubesvoppg') {echo "selected";}}?>>Ubesvarte oppgaver</option>
-                <option class="dropned"  name="pbegoppg" value='pbegoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'pbegoppg') {echo "selected";}}?>>P&aringbegynte oppgaver</option>
-                <option class="dropned" name="besvoppg" value='besvoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'besvoppg') {echo "selected";}}?>>Besvarte oppgaver</option>
-                <option class="dropned" name="besvmresp" value='besvmresp' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'besvmresp') {echo "selected";}}?>>Besvarelser med respons</option>
-            </select></center><br>
+           <!--Nedtrekksmeny for å bytte mellom de forskjellige oppgavelistene på brukerens profil !-->
+           <form class="fasplas"action="minside.php" method="post">
+              <select class="dropned" name='minsideoppgli' onchange="this.form.submit();">
+                  <option class="dropned" name="ubesvoppg" value='ubesvoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'ubesvoppg') {echo "selected";}}?>>Ubesvarte oppgaver</option>
+                  <option class="dropned"  name="pbegoppg" value='pbegoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'pbegoppg') {echo "selected";}}?>>Påbegynte oppgaver</option>
+                  <option class="dropned" name="besvoppg" value='besvoppg' <?php if(isset($_POST['minsideoppgli'])) { if($_POST['minsideoppgli'] == 'besvoppg') {echo "selected";}}?>>Besvarte oppgaver</option>
+              </select></center><br>
             </form>
-
-
+             <!--Felter for å bytte passord på brukerens profil !-->
             <div id="minside">
                 <form id="byttpw" name="reg" method="post" action="minside.php">
                  <h2>Bytt passord</h2>
@@ -123,17 +115,17 @@ if($user_data['passord'] != $gammeltpw) {
                     Bekreft nytt passord: 
                     <br><input type="password" id="bekreft" name="passwordcheck" placeholder="Bekreft nytt passord"/><br /><br />
                     <input type="Submit" id="pwknapp" name="nypw" value="Bekreft">
-                    <br><?php 
+                    <br>
+                    <?php 
+                    //Skriver ut statusmeldiner i forbindelse med passordbytte
                     if (empty($errors) === false) {
                         echo output_errors($errors);
                     } ?>
                 </form>
-                
- 
             </div>
              <div class="ubesform2">
               <?php 
-
+              //Setter paramterene for riktig liste som skal vises basert på valget i nedtrekksmenyen
               $bPK = $user_data['brukerPK'];
             if(!empty($_POST['minsideoppgli'])) {
                 if($_POST['minsideoppgli'] == 'ubesvoppg') {
@@ -142,8 +134,6 @@ if($user_data['passord'] != $gammeltpw) {
                   $result = ubesvarteOppg($bPK, 0);
                 } elseif($_POST['minsideoppgli'] == 'besvoppg') {
                   $result = ubesvarteOppg($bPK, 1);
-                } elseif($_POST['minsideoppgli'] == 'besvmresp') {
-                  $result = ubesvarteOppg($bPK, 2);
                 }  else {
                     $result = ubesvarteOppg($bPK, 3);
                 }
@@ -152,9 +142,10 @@ if($user_data['passord'] != $gammeltpw) {
                 }
                 include_once("ubesvartliste.php");
                ?>
-
           </div>
+           <!--Liste over besvarte oppgaver med respons, på brukerens profil !-->
           <div class="minsidemainnede">
+          <h3>Besvarte oppgaver med respons</h3>
             <?php include_once("besvart.php"); ?>
           </div>
        </section>
